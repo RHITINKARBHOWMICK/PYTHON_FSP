d7b618966bf1c9f51eff2646bb78b8d568173cef
RHITINKARBHOWMICK <rhitbhowmick2005@gmail.com>
2025-04-23 16:43:29 +0530

    Add files via upload

---
+++ dict1.py
@@ -0,0 +1,4 @@
+#highest 3 values of the dictionary
+d={1:23,2:34,3:30,4:45,5:56}
+print(sorted(d.values(),reverse=True)[:3])
+    
---
+++ dictionary.py
@@ -0,0 +1,11 @@
+d={3:45,2:70,4:77,5:40}
+print(d[2])
+d[4]=45
+print(d)
+
+d[7]=90
+print(d)
+print(d.values())
+print(d.keys())
+for i in d.values():
+     print(i)
